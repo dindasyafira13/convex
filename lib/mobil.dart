2da3fc88342d7f858e1a80afa85168d7888b852d
Client-4 <dindasyafira703@gmail.com>
2023-01-04 10:33:28 +0700

    dindasyafira13

---
+++ lib/mobil.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Mobil extends StatefulWidget {
+  const Mobil({super.key});
+
+  @override
+  State<Mobil> createState() => _MobilState();
+}
+
+class _MobilState extends State<Mobil> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Page Mobil') ,
+        
+        
+        ),
+    );
+  }
+}
